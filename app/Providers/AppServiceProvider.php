7ea0f086682fe934e950d5a99179790b0209af00
Mariano Escalera <mariano.pualiu@gmail.com>
2026-08-12 02:48:32 -0600

    Forcing HTTPS protocol

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production') || str_starts_with((string) config('app.url'), 'https://')) {
+        if ($this->app->environment() !== 'local' || isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
             URL::forceScheme('https');
         }
     }
