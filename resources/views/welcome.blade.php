deb04783db4c4e5ddaec834ef6ffb53ebebd3cdd
Mariano Escalera <mariano.pualiu@gmail.com>
2026-08-12 00:55:57 -0600

    Fixed incorrect CSS welcome page reference

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>{{ config('app.name', 'Prueba Técnica') }}</title>
 
         <!-- Styles / Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     </head>
     <body class="bg-light text-dark min-vh-100 d-flex flex-column">
